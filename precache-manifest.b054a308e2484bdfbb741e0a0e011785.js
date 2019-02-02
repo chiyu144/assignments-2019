6bceed510709499c565c897c118b83adf13654ab
Julie <sizumasa@hotmail.com>
2019-02-02 15:43:04 +0800

    Updates

---
+++ precache-manifest.b054a308e2484bdfbb741e0a0e011785.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "48f87712689e440584e452b5b55d1988",
+    "url": "/remote-assignments/week-4/dbc/static/media/breakfast.48f87712.png"
+  },
+  {
+    "revision": "ce9a0e597e8a83a7903b",
+    "url": "/remote-assignments/week-4/dbc/static/css/main.6b77a98c.chunk.css"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/remote-assignments/week-4/dbc/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "708ead6f21aa0c163e8d",
+    "url": "/remote-assignments/week-4/dbc/static/js/runtime~main.708ead6f.js"
+  },
+  {
+    "revision": "a9566b84d33dd68de614c40e027be576",
+    "url": "/remote-assignments/week-4/dbc/static/media/logo.a9566b84.png"
+  },
+  {
+    "revision": "57433379b0608e2ac0e5e9e8350a0cbc",
+    "url": "/remote-assignments/week-4/dbc/static/media/fullstack.57433379.png"
+  },
+  {
+    "revision": "49e3793b028850c929c7fff888527f1d",
+    "url": "/remote-assignments/week-4/dbc/static/media/jellyfish.49e3793b.png"
+  },
+  {
+    "revision": "ce9a0e597e8a83a7903b",
+    "url": "/remote-assignments/week-4/dbc/static/js/main.ce9a0e59.chunk.js"
+  },
+  {
+    "revision": "f1a98cdb68312eadcee493dffd03c1f9",
+    "url": "/remote-assignments/week-4/dbc/static/media/cat.f1a98cdb.png"
+  },
+  {
+    "revision": "7569d220f26eb9f56d9af9e8331912ef",
+    "url": "/remote-assignments/week-4/dbc/static/media/yen.7569d220.png"
+  },
+  {
+    "revision": "d4eaefae9ecd5fc3aea14539820dbd19",
+    "url": "/remote-assignments/week-4/dbc/static/media/blossom.d4eaefae.png"
+  },
+  {
+    "revision": "7993930288e5c9caa62d74d9c63f6aa7",
+    "url": "/remote-assignments/week-4/dbc/static/media/sunlit.79939302.png"
+  },
+  {
+    "revision": "82775eceb3b803b04c5faece15d50390",
+    "url": "/remote-assignments/week-4/dbc/static/media/squar.82775ece.png"
+  },
+  {
+    "revision": "4b66c51ca89cca157075356089bad73f",
+    "url": "/remote-assignments/week-4/dbc/index.html"
+  }
+];
